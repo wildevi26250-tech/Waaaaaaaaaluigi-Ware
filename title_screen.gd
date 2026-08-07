55f1315f5a4992791477fedce915496385714bfb
wildevi26250-tech <wildevi26250@whs.school.nz>
2026-08-07 17:30:59 +1200

    Added some basic code
    added functioning buttons to the title screen

---
+++ title_screen.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://level_scene.tscn")
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_settings_pressed() -> void:
+	get_tree().change_scene_to_file("res://settings_scene.tscn.tscn")
